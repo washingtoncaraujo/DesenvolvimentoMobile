b95633d3c1c45b30fe180dd9dd29e00e104ee471
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-09-19 17:13:54 -0300

    teste 6

--- DesafioSemana6/Questao6.dart
+++ DesafioSemana6/Questao6.dart
@@ -8,16 +8,36 @@ bool canConstruct(String bilhete, String oraculo) {
   if (ref.length < bilhe.length){
     return false;
   } else {
-    for (int i=0; i<tref; i++){
+    for (int i=0; i<=tref; i++){
       for (int j=0; j< tbilhe; j++){
-        if (bilhe[j] == ref[i]) {
-          ref.removeAt(i);
-          ++i;
-          bilhe.removeAt(j);
-          --j;
-          print(bilhe.length);
+        if(ref[i] == bilhe[j]){
+          do {
+            ref.removeAt(i);
+            tref = ref.length;
+            bilhe.removeAt(j);
+            tbilhe = bilhe.length;
+            if(bilhe.length == 0) return true;
+          } while(ref[i] == bilhe[j]);
+          print(bilhe);
+          
         }
-      }      
+       /*if (bilhe.length == 1) j = bilhe.length-1;
+       if (bilhe[j] == ref[i]) {
+         ref.removeAt(i);
+         tref = ref.length;
+         if (i != 0) --i;
+          print(ref);
+          print(i);
+          bilhe.removeAt(j);
+          tbilhe = bilhe.length;
+          if (j != 0) --j;
+          if(bilhe[j] == ref[i]){
+          }
+          print(bilhe);
+          print(j);
+          if(bilhe.length == 0) return true;
+        }*/
+      }   
     }
     if (bilhe.length == 0){
       return true;
@@ -33,3 +53,4 @@ void main() {
   List strings = lista.split("*");
   print(canConstruct(strings[0], strings[1]));
 }
+
