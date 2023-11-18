6dc5b5da3a053ee7ffe4ab77264e6f7ac587516b
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-11-18 03:24:24 -0300

    iniciado projeto crud

---
+++ Semana5/CRUD/crud_app/lib/models/client.dart
@@ -0,0 +1,14 @@
+class Client {
+ final String id;
+ final String nome;
+ final String sobrenome;
+ final String email;
+ final String avatarUrl;
+ const Client({
+ required this.id,
+ required this.nome,
+ required this.sobrenome,
+ required this.email,
+ required this.avatarUrl,
+ });
+}
