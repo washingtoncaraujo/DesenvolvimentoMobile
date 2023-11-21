faef3cb9ea448819c19179920946aede0c39a04f
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-11-20 21:48:27 -0300

    iniciando desafio

--- ModuloII/Semana5/ClienteBD/src/main/java/dev/br/clientebd/controller/ClienteController.java
+++ ModuloII/Semana5/ClienteBD/src/main/java/dev/br/clientebd/controller/ClienteController.java
@@ -2,10 +2,7 @@ package dev.br.clientebd.controller;
 
 import dev.br.clientebd.model.Cliente;
 import dev.br.clientebd.repository.ClienteRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +25,26 @@ public class ClienteController {
     @GetMapping("/{id}") // Mapeia o método para responder a requisições GET com um parâmetro de caminho (ID).
     public Cliente getClienteById(@PathVariable Long id) {
         return ClienteRepository.findById(id).orElse(null); // Busca um produto pelo ID.
+    }
+
+    @PostMapping // Mapeia o método para responder a requisições POST.
+    public Cliente createCliente(@RequestBody Cliente cliente){
+        return ClienteRepository.save(cliente); // Cria um novo cliente no banco de dados.
+    }
+
+    @PutMapping("/{id}") // Mapeia o método para responder a requisições PUT com um parâmetro de caminho (ID).
+    public Cliente updateCategory(@PathVariable Long id, @RequestBody Cliente updatedCliente){
+        Cliente existingCliente = ClienteRepository.findById(id).orElse(null); // Busca a categoria existente pelo ID.
+        if (existingCliente != null){
+            existingCliente.setName(updatedCliente.getName()); // Atualiza o nome da categoria.
+            return ClienteRepository.save(existingCliente); // Salva a categoria atualizada no banco de dados.
+        }
+        return null; // Retorna nulo se a categoria não for encontrada.
+    }
+
+    @DeleteMapping("/{id}") // Mapeia o método para responder a requisições DELETE com um parâmetro de caminho (ID).
+    public void deleteCliente(@PathVariable Long id){
+        ClienteRepository.deleteById(id); // Exclui uma categoria pelo ID.
     }
 
 }
