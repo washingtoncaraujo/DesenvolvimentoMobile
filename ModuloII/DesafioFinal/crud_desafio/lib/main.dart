ed5edb2a5bd45da3b4b3bf7b5f149ab3775db6c5
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-11-22 16:29:43 -0300

    titulo da aplicação alterado

--- ModuloII/DesafioFinal/crud_desafio/lib/main.dart
+++ ModuloII/DesafioFinal/crud_desafio/lib/main.dart
@@ -6,8 +6,9 @@ import 'views/clients_list.dart';
 import 'views/formulaire.dart';
 
 
+
 void main() {
- runApp(const MyApp());
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       ChangeNotifierProvider(create: (ctx) => ClientProvider())
     ],
     child: MaterialApp(
-    title: 'Desafio Final',
+    title: 'Desafio CRUD',
     theme: ThemeData(
     appBarTheme: const AppBarTheme(backgroundColor: Color.fromARGB(103, 15, 15, 199)),
     scaffoldBackgroundColor: Color.fromARGB(121, 206, 229, 240),
